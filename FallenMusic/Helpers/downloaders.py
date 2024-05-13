adba0e74a3087e5c98a2dad5eb0749440437eb2a
Noxiousxnoob <154877712+Noxiousxnoob@users.noreply.github.com>
2024-05-13 17:15:25 +0530

    Update downloaders.py
    Signed-off-by: Noxiousxnoob <154877712+Noxiousxnoob@users.noreply.github.com>

--- FallenMusic/Helpers/downloaders.py
+++ FallenMusic/Helpers/downloaders.py
@@ -44,7 +44,7 @@ ydl = YoutubeDL(ydl_opts)
 
 
 def audio_dl(url: str) -> str:
-    sin = ydl.extract_info(url, False)
+    sin = ydl.extract_info(url, True)
     x_file = os.path.join("downloads", f"{sin['id']}.mp3")
     if os.path.exists(x_file):
         return x_file
